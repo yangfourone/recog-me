58c90010198278d54f428605b5424ed2ff5b4cae
yangfourone <32697995+yangfourone@users.noreply.github.com>
2020-04-09 14:36:50 +0800

    showing location tag with direction in indoor map

--- thesis/Recognize.swift
+++ thesis/Recognize.swift
@@ -9,8 +9,9 @@
 import UIKit
 import AVFoundation
 import CoreMotion
+import CoreLocation
 
-class Recognize: UIViewController {
+class Recognize: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var cameraView: UIImageView!
     @IBOutlet weak var indoorMap: UIImageView!
@@ -23,6 +24,143 @@ class Recognize: UIViewController {
     
     var timerTask:Timer?
     let altimeter = CMAltimeter()
+    var hPa:Float = 0.0
+    var counterForBarometer = 0
+    var valueForBarometer:Float = 0.0
+    
+    var LM = CLLocationManager()
+    
+    var directionTag:UIImageView?
+    
+    // MARK: Tag Information Definition
+    let tagInformation:[Dictionary<String,Any>] = [
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "A",
+            "x-axis" : 805,
+            "y-axis" : 696
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "B",
+            "x-axis" : 730,
+            "y-axis" : 696
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "C",
+            "x-axis" : 679,
+            "y-axis" : 696
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "D",
+            "x-axis" : 626,
+            "y-axis" : 749
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "E",
+            "x-axis" : 626,
+            "y-axis" : 696
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "F",
+            "x-axis" : 626,
+            "y-axis" : 677
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "G",
+            "x-axis" : 559,
+            "y-axis" : 677
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "H",
+            "x-axis" : 507,
+            "y-axis" : 677
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "I",
+            "x-axis" : 425,
+            "y-axis" : 677
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "J",
+            "x-axis" : 340,
+            "y-axis" : 677
+        ],
+        [
+            "building" : "EE",
+            "floor" : "7F",
+            "position" : "K",
+            "x-axis" : 340,
+            "y-axis" : 749
+        ],
+        [
+            "building" : "EE",
+            "floor" : "8F",
+            "position" : "A",
+            "x-axis" : 681,
+            "y-axis" : 696
+        ],
+        [
+            "building" : "EE",
+            "floor" : "8F",
+            "position" : "B",
+            "x-axis" : 625,
+            "y-axis" : 750
+        ],
+        [
+            "building" : "EE",
+            "floor" : "8F",
+            "position" : "C",
+            "x-axis" : 625,
+            "y-axis" : 689
+        ],
+        [
+            "building" : "EE",
+            "floor" : "8F",
+            "position" : "D",
+            "x-axis" : 560,
+            "y-axis" : 689
+        ],
+        [
+            "building" : "EE",
+            "floor" : "8F",
+            "position" : "E",
+            "x-axis" : 468,
+            "y-axis" : 689
+        ],
+        [
+            "building" : "EE",
+            "floor" : "8F",
+            "position" : "F",
+            "x-axis" : 409,
+            "y-axis" : 689
+        ],
+        [
+            "building" : "EE",
+            "floor" : "8F",
+            "position" : "G",
+            "x-axis" : 328,
+            "y-axis" : 689
+        ]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,14 +178,22 @@ class Recognize: UIViewController {
         indoorMapMask.layer.borderWidth = 3
         indoorMapMask.backgroundColor = UIColor(white: 0, alpha: 1)
         
-//        startRelativeAltitudeUpdates()
+        startRelativeAltitudeUpdates()
         activity.stopAnimating()
+        
+        /** location manager **/
+        LM.requestWhenInUseAuthorization()
+        LM.requestAlwaysAuthorization()
+        LM.delegate = self
+        LM.startUpdatingLocation()
     }
     
+    // MARK: Log Out Button
     @IBAction func logOut(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    // MARK: Start Button
     @IBAction func Start(_ sender: Any) {
         /** UI update **/
         cameraViewMask.backgroundColor = UIColor(white: 0, alpha: 0)
@@ -62,9 +208,10 @@ class Recognize: UIViewController {
         CaptureManager.shared.startSession()
         CaptureManager.shared.delegate = self
         
-        timerTask = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: Selector(("Recognize")), userInfo: nil, repeats: true)
+        timerTask = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(self.Recognize), userInfo: nil, repeats: true)
     }
     
+    // MARK: End Button
     @IBAction func End(_ sender: Any) {
         /** UI update **/
         cameraViewMask.backgroundColor = UIColor(white: 0, alpha: 1)
@@ -84,6 +231,7 @@ class Recognize: UIViewController {
         timerTask = nil
     }
     
+    // MARK: Upload and Get Response
     @objc func Recognize() {
         uploadImage { results in
             switch results {
@@ -95,7 +243,7 @@ class Recognize: UIViewController {
                 do {
                     if let jsonArray = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [Dictionary<String,Any>]
                     {
-                        self.floorRecognize(jsonArray: jsonArray)
+                        self.floorRecognize(jsonArray: jsonArray, pressure: self.hPa)
                     } else {
                         print("bad json")
                     }
@@ -113,28 +261,75 @@ class Recognize: UIViewController {
         }
     }
     
+    // MARK: 氣壓偵測與相對高度偵測
     func startRelativeAltitudeUpdates() {
-        guard CMAltimeter.isRelativeAltitudeAvailable() else {
-            print("\n當前設備不支援獲取高度\n")
-            return
-        }
-
-        let queue = OperationQueue.current
-        self.altimeter.startRelativeAltitudeUpdates(to: queue!, withHandler: {
-            (altitudeData, error) in
-            guard error == nil else {
-                print(error!)
-                return
-            }
-            // 千帕
-            print("\n\(altitudeData!.pressure) kPa")
-            // 百帕
-            print("\(Float(truncating: altitudeData!.pressure)*10.0) hPa")
-            // 相對高度
-            print("\(altitudeData!.relativeAltitude) m")
-        })
+//        guard CMAltimeter.isRelativeAltitudeAvailable() else {
+//            print("\n當前設備不支援獲取高度\n")
+//            return
+//        }
+//
+//        let queue = OperationQueue.current
+//        self.altimeter.startRelativeAltitudeUpdates(to: queue!, withHandler: {
+//            (altitudeData, error) in
+//            guard error == nil else {
+//                print(error!)
+//                return
+//            }
+//            // 百帕
+//            self.hPa = Float(truncating: altitudeData!.pressure)*10.0
+//
+//            self.counterForBarometer += 1
+//            self.valueForBarometer += self.hPa
+//            let averageBarometerValue = self.valueForBarometer / Float(self.counterForBarometer)
+//            self.information.text = "Times: \(self.counterForBarometer), Average: \(averageBarometerValue)"
+//
+//            // 相對高度
+//            print("\(altitudeData!.relativeAltitude) m\n")
+//        })
     }
     
+    // MARK: Floor Recognize ??
+    func floorRecognize(jsonArray: [Dictionary<String,Any>], pressure: Float) {
+        let index = 0
+        indoorMap.image = UIImage(named: "\(jsonArray[index]["floor"]! as! String).png")
+        setResponseParameter(building: jsonArray[index]["building"]! as! String, floor: jsonArray[index]["floor"]! as! String, position: jsonArray[index]["position"]! as! String, degree: jsonArray[index]["degree"]! as! String, chineseLabel: jsonArray[index]["chinese"]! as! String)
+    }
+    
+    // MARK: Show Parameters to Label
+    func setResponseParameter(building: String, floor: String, position: String, degree: String, chineseLabel: String) {
+        getTagPosition(building: building, floor: floor, position: position, degree: degree)
+        information.text = "\(building) \(floor) \(position) \(degree) \(chineseLabel)"
+    }
+    
+    // MARK: Get Tag x-axis and y-axis
+    func getTagPosition (building: String, floor: String, position: String, degree: String) {
+        for index in 0..<tagInformation.count {
+            if building == tagInformation[index]["building"] as! String && floor == tagInformation[index]["floor"] as! String && position == tagInformation[index]["position"] as! String {
+                showTag(x: tagInformation[index]["x-axis"] as! Int, y: tagInformation[index]["y-axis"] as! Int, degree: degree)
+            }
+        }
+    }
+    
+    // MARK: Show The Tag
+    func showTag(x:Int, y:Int, degree:String) {
+        if directionTag != nil {
+            directionTag?.removeFromSuperview()
+        }
+        let imageName = "\(degree).png"
+        let image = UIImage(named: imageName)
+        directionTag = UIImageView(image: image!)
+        directionTag!.frame = CGRect(x: x, y: y, width: 15, height: 15)
+        view.addSubview(directionTag!)
+    }
+    
+    // MARK: GPS Location Information (altitude)
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+//            print("高度: \(location.altitude)\n")
+        }
+    }
+    
+    // MARK: Upload Image
     func uploadImage(completion: @escaping (Result<String, Error>) -> Void) {
         /** UI **/
         activity.startAnimating()
@@ -169,7 +364,7 @@ class Recognize: UIViewController {
         }.resume()
     }
     
-    /** resize image **/
+    // MARK: Resize Image
     func resizeImage(image: UIImage, width: CGFloat) -> UIImage {
             let size = CGSize(width: width, height:
                 image.size.height * width / image.size.width)
@@ -180,17 +375,7 @@ class Recognize: UIViewController {
             return newImage
     }
     
-    /** floor recognize **/
-    func floorRecognize(jsonArray: [Dictionary<String,Any>]) {
-        setResponseParameter(building: jsonArray[0]["building"]! as! String, floor: jsonArray[0]["floor"]! as! String, position: jsonArray[0]["position"]! as! String, degree: jsonArray[0]["degree"]! as! String, chineseLabel: jsonArray[0]["chinese"]! as! String)
-    }
-    
-    /** parameters **/
-    func setResponseParameter(building: String, floor: String, position: String, degree: String, chineseLabel: String) {
-        information.text = "\(building) \(floor) \(position) \(degree) \(chineseLabel)"
-    }
-    
-    /** save image to album **/
+    // MARK: Save Image To Album
     func saveImage(image: UIImage) {
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
     }
